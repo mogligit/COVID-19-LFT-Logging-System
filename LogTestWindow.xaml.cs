5bd8a2154a2faaf97aa58f7fc626ac23909c82ef
mogligit <60526835+mogligit@users.noreply.github.com>
2021-03-21 23:22:09 +0000

    created Log Test Window

---
+++ LogTestWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace COVID_19_LFT_Logging_System
+{
+    /// <summary>
+    /// Interaction logic for LogTestWindow.xaml
+    /// </summary>
+    public partial class LogTestWindow : Window
+    {
+        public LogTestWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
