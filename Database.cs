75dabf0cb8aca70e0bdcb62b97786ea9bd659992
mogligit <60526835+mogligit@users.noreply.github.com>
2021-03-23 00:07:25 +0000

    created Database class for easy operations on DB

---
+++ Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace COVID_19_LFT_Logging_System
+{
+    class Database
+    {
+        
+        private const string DATABASE_PATH = "C:\\Users\\mogli\\source\\repos\\COVID-19 LFT Logging System\\TestDB.mdf";
+        public static void Connect()
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"" + DATABASE_PATH + "\";Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+        }
+    }
+}
