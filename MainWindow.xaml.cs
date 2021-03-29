5158ce6a96bd1c1a8b9f355ad516792cbe492364
mogligit <60526835+mogligit@users.noreply.github.com>
2021-03-29 21:37:04 +0100

    implemented very badly coded dynamic database creation if database is not found

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,21 +29,27 @@ namespace COVID_19_LFT_Logging_System
         // Test connection to database at load up
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            try
+            
+            // check if database exists, and create one
+            string workingDir = Directory.GetCurrentDirectory();
+            string databasePath = workingDir + "\\TestDB";
+            databasePath = "DeleteDB";
+
+            if (!Database.TryConnect(databasePath))
             {
-                Database.Connect();
+                Database.CreateNewDatabase(databasePath);
+                Database.TryConnect(databasePath);
                 lblDBStatus.Content = "Database Online";
                 lblDBStatus.Background = new SolidColorBrush(Color.FromRgb(51, 204, 51));
-
             }
-            catch
+            else
             {
                 lblDBStatus.Content = "Database Error";
                 lblDBStatus.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
                 // Prompt for new database or alternative one
                 MessageBox.Show("Database could not be found.");
             }
-            
+                       
         }
 
         private void btnLogTests_Click(object sender, RoutedEventArgs e)
